ce44671db1110e58b4a3017b1eaed85106638c3e
Motaz-Mukhtar <motazksa2@gmail.com>
2023-02-17 14:38:18 -0800

    Fix the pycodestyle for app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -11,6 +11,7 @@ app = Flask(__name__)
 app.register_blueprint(app_views, url_prefix="/api/v1")
 cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
+
 @app.teardown_appcontext
 def close_storage(x):
     """close storage"""
